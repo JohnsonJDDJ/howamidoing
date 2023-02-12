79028a47ae2de1b28ba95c8651258555b1e5960f
Johnson Du <zhihao617@berkeley.edu>
2023-02-11 23:28:48 -0800

    templates for auth

--- src/howamidoing/__init__.py
+++ src/howamidoing/__init__.py
@@ -1,19 +1,14 @@
 __version__ = "0.0.1"
 
-import os
-
 from .objects import *
 from .utils import *
 from flask import Flask
-from pymongo import MongoClient
-from flask_pymongo import PyMongo
-
-
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     app.config["MONGO_URI"] = "mongodb://localhost:27017/howamidoing"
+    app.secret_key = "secret_key_value_goes_here"
 
     from . import demo
     app.register_blueprint(demo.bp)
